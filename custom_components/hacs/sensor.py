32571a0f6854bdfa6f4a4074c989506061af066e
Joakim Sørensen <joasoe@gmail.com>
2019-06-08 08:52:21 +0200

    Adds a sensor to show number of pending updates (#61)
    * Initial sensor
    
    * fix typo
    
    * Adds unit_of_measurement
    
    * rewording

---
+++ custom_components/hacs/sensor.py
@@ -0,0 +1,52 @@
+"""Sensor platform for HACS."""
+from homeassistant.helpers.entity import Entity
+from . import hacs
+
+
+async def async_setup_platform(
+        hass, config, async_add_entities, discovery_info=None):  # pylint: disable=unused-argument
+    """Setup sensor platform."""
+    async_add_entities([HACSSensor()])
+
+
+class HACSSensor(Entity):
+    """HACS Sensor class."""
+
+    def __init__(self):
+        """Initialize."""
+        self._state = None
+
+    async def async_update(self):
+        """Update the sensor."""
+        # Send update "signal" to the component
+        if hacs.data["task_running"]:
+            return
+
+        updates = 0
+
+        for repository in hacs.repositories:
+            repository = hacs.repositories[repository]
+            if repository.pending_update:
+                updates += 1
+
+        self._state = updates
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return "hacs"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return "mdi:package"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return "pending update(s)"

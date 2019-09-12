b3d1512db264cf57a0916f1ab3a92c39272ad4a2
Joakim Sørensen <ludeeus@gmail.com>
2019-09-12 18:21:18 +0200

    Fix 'HacsManifest' object is not subscriptable (#519)

--- custom_components/hacs/repositories/manifest.py
+++ custom_components/hacs/repositories/manifest.py
@@ -18,34 +18,41 @@ class HacsManifest:
         """Return a bool to indicate that the content is in the roop of the repository."""
         return self.manifest.get("content_in_root")
 
+    @property
     def filename(self):
         """Return the filename."""
         return self.manifest.get("filename")
 
+    @property
     def domains(self):
         """Return the domains."""
         if isinstance(self.manifest.get("domains", []), str):
             return [self.manifest.get("domains", [])]
         return self.manifest.get("domains", [])
 
+    @property
     def country(self):
         """Return the country."""
         if isinstance(self.manifest.get("country", []), str):
             return [self.manifest.get("country", [])]
         return self.manifest.get("country", [])
 
+    @property
     def homeassistant(self):
         """Return the minimum homeassistant version."""
         return self.manifest.get("homeassistant")
 
+    @property
     def persistent_directory(self):
         """Return the persistent_directory."""
         return self.manifest.get("persistent_directory")
 
+    @property
     def iot_class(self):
         """Return the iot_class."""
         return self.manifest.get("iot_class")
 
+    @property
     def render_readme(self):
         """Return a bool to indicate that the readme file should be rendered."""
         return self.manifest.get("render_readme")

e2e8f68267485710fce45bd8e5ab9a59bb6b438f
ludeeus <joasoe@gmail.com>
2019-10-30 16:20:57 +0000

    Adds new helper version_is_newer_than_version

--- custom_components/hacs/helpers/misc.py
+++ custom_components/hacs/helpers/misc.py
@@ -1,4 +1,6 @@
 """Helper functions: misc"""
+# from distutils.version import LooseVersion
+import semantic_version
 from custom_components.hacs.repositories.manifest import HacsManifest
 
 
@@ -19,3 +21,10 @@ def get_repository_name(
                 return manifest["name"]
 
     return repository_name.replace("-", " ").replace("_", " ").title()
+
+
+def version_is_newer_than_version(new: str, old: str) -> bool:
+    """Return a bool if source is newer than target, will also be true if identical."""
+    if new == old:
+        return True
+    return semantic_version.Version.coerce(new) > semantic_version.Version.coerce(old)
---
+++ tests/test_helper_misc_version_is_newer_than_version.py
@@ -0,0 +1,27 @@
+"""Helpers: Misc: version_is_newer_than_version."""
+# pylint: disable=missing-docstring
+from custom_components.hacs.helpers.misc import version_is_newer_than_version
+
+
+def test_basic():
+    assert version_is_newer_than_version("1.0.0", "0.9.9")
+    assert version_is_newer_than_version("1", "0.9.9")
+    assert version_is_newer_than_version("1.1", "0.9.9")
+    assert version_is_newer_than_version("0.10.0", "0.9.9")
+    assert not version_is_newer_than_version("0.0.10", "0.9.9")
+    assert not version_is_newer_than_version("0.9.0", "0.9.9")
+    assert version_is_newer_than_version("1.0.0", "1.0.0")
+
+
+def test_beta():
+    assert version_is_newer_than_version("1.0.0b1", "1.0.0b0")
+    assert not version_is_newer_than_version("1.0.0b1", "1.0.0")
+    assert version_is_newer_than_version("1.0.0", "1.0.0b1")
+
+
+def test_wierd_stuff():
+    assert version_is_newer_than_version("1.0.0rc1", "1.0.0b1")
+    assert not version_is_newer_than_version("1.0.0a1", "1.0.0b1")
+    assert version_is_newer_than_version("1.0.0", "1.0.0a0")
+    assert version_is_newer_than_version("1.0.0", "1.0.0b0")
+    assert version_is_newer_than_version("1.0.0", "1.0.0rc0")

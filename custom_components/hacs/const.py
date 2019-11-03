7d39ef3a20421d2057679d824bb73ae880bc6dfb
ludeeus <joasoe@gmail.com>
2019-11-03 15:35:26 +0000

    Set version to master

--- custom_components/hacs/const.py
+++ custom_components/hacs/const.py
@@ -1,7 +1,7 @@
 """Constants for HACS"""
 NAME_LONG = "HACS (Home Assistant Community Store)"
 NAME_SHORT = "HACS"
-VERSION = "0.16.3"
+VERSION = "master"
 DOMAIN = "hacs"
 PROJECT_URL = "https://github.com/hacs/integration/"
 CUSTOM_UPDATER_LOCATIONS = [

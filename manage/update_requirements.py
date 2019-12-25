b1eb61601faff3df7c506690ee4b12bd8094dac1
ludeeus <joasoe@gmail.com>
2019-12-25 20:11:42 +0000

    Update requirements handling

---
+++ manage/update_requirements.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+with open(f"{os.getcwd()}/custom_components/hacs/manifest.json", "r") as manifest:
+    manifest = json.load(manifest)
+    requirements = []
+    for req in manifest["requirements"]:
+        requirements.append(req.split(">")[0].split("=")[0])
+    manifest["requirements"] = requirements
+
+with open(f"{os.getcwd()}/requirements.txt", "r") as requirements:
+    tmp = requirements.readlines()
+    requirements = []
+    for req in tmp:
+        requirements.append(req.replace("\n", ""))
+
+for req in requirements:
+    if req.split(">")[0].split("=")[0] in manifest["requirements"]:
+        manifest["requirements"].remove(req.split(">")[0].split("=")[0])
+        manifest["requirements"].append(req)
+
+
+with open(f"{os.getcwd()}/custom_components/hacs/manifest.json", "w") as manifestfile:
+    manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))

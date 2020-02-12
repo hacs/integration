9d1f1ec8b07d2699d7e12097276f6342e0b9e439
ludeeus <joasoe@gmail.com>
2020-02-12 17:32:19 +0000

    Fix AttributeError

--- custom_components/hacs/helpers/download.py
+++ custom_components/hacs/helpers/download.py
@@ -14,22 +14,27 @@ class FileInformation:
         self.name = name
 
 
+def should_try_releases(repository):
+    """Return a boolean indicating whether to download releases or not."""
+    if repository.ref == repository.information.default_branch:
+        return False
+    if repository.information.category not in ["plugin", "theme"]:
+        return False
+    if not repository.releases.releases:
+        return False
+    return True
+
+
 def gather_files_to_download(repository):
     """Return a list of file objects to be downloaded."""
     files = []
     tree = repository.tree
-    ref = repository.ref.replace("tags/", "")
-    default = repository.information.default_branch
+    ref = f"{repository.ref}".replace("tags/", "")
     releaseobjects = repository.releases.objects
     category = repository.information.category
     remotelocation = repository.content.path.remote
 
-    if (
-        repository.releases.releases
-        and releaseobjects
-        and category in ["plugin", "theme"]
-        and ref != default
-    ):
+    if should_try_releases(repository):
         for release in releaseobjects or []:
             if ref == release.tag_name:
                 for asset in release.assets or []:

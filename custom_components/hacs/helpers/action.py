42696ad4dde27db6d6fc62bce08e4dd2767287b0
ludeeus <joasoe@gmail.com>
2020-05-08 17:53:28 +0000

    Add brands

--- custom_components/hacs/helpers/action.py
+++ custom_components/hacs/helpers/action.py
@@ -2,12 +2,21 @@
 from custom_components.hacs.hacsbase.exceptions import HacsException
 from custom_components.hacs.helpers.information import get_tree
 
+BRANDS_REPO = "https://github.com/home-assistant/brands"
 WHEEL_REPO = "https://github.com/home-assistant/wheels-custom-integrations"
 
 
 async def run_action_checks(repository):
     """Checks to run as an action."""
     brands = await repository.hacs.github.get_repo("home-assistant/brands")
+    brandstree = await get_tree(brands, "master")
+    if (
+        f"custom_integrations/{repository.integration_manifest['domain']}/icon.png"
+        not in [x.filename for x in brandstree]
+    ):
+        raise HacsException(f"Integration not added to {BRANDS_REPO}")
+    repository.logger.info(f"Integration is added to {BRANDS_REPO}, nice!")
+
     if (
         repository.integration_manifest.get("requirements") is not None
         and len(repository.integration_manifest.get("requirements")) != 0

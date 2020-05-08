685703602e61040735c59e297bb3a45f44bc9f44
ludeeus <joasoe@gmail.com>
2020-05-08 18:00:05 +0000

    fix brands check

--- custom_components/hacs/helpers/action.py
+++ custom_components/hacs/helpers/action.py
@@ -10,12 +10,10 @@ async def run_action_checks(repository):
     """Checks to run as an action."""
     brands = await repository.hacs.github.get_repo("home-assistant/brands")
     brandstree = await get_tree(brands, "master")
-    repository.logger.info([x.filename for x in brandstree])
-    repository.logger.info(repository.integration_manifest)
-    if (
-        f"custom_integrations/{repository.integration_manifest['domain']}/icon.png"
-        not in [x.filename for x in brandstree]
-    ):
+    repository.logger.info(repository.integration_manifest.get("requirements"))
+    if repository.integration_manifest["domain"] not in [
+        x.filename for x in brandstree
+    ]:
         raise HacsException(f"Integration not added to {BRANDS_REPO}")
     repository.logger.info(f"Integration is added to {BRANDS_REPO}, nice!")
 
@@ -28,7 +26,7 @@ async def run_action_checks(repository):
         )
         wheeltree = await get_tree(wheels, "master")
         repository.logger.info([x.filename for x in brandstree])
-        if f"components/{repository.integration_manifest['domain']}.jsons" not in [
+        if f"{repository.integration_manifest['domain']}.jsons" not in [
             x.filename for x in wheeltree
         ]:
             raise HacsException(f"Integration not added to {WHEEL_REPO}")

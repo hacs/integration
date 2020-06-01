01ed61c642848d09e890058a5edb81c223e9a7bc
ludeeus <joasoe@gmail.com>
2020-06-01 16:17:14 +0000

    Add test for clear_storage

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -3,10 +3,10 @@
 import json
 import aiohttp
 import pytest
-
+import os
 from custom_components.hacs.globals import get_hacs
 from custom_components.hacs.hacsbase.configuration import Configuration
-from custom_components.hacs.setup import load_hacs_repository
+from custom_components.hacs.setup import load_hacs_repository, clear_storage
 from tests.sample_data import (
     response_rate_limit_header,
     repository_data,
@@ -15,6 +15,17 @@ from tests.sample_data import (
 )
 
 TOKEN = "xxxxxxxxxxxxxxxxxxxxxxx"
+
+
+def test_clear_storage(tmpdir):
+    hacs = get_hacs()
+    hacs.system.config_path = tmpdir.dirname
+    os.makedirs(f"{hacs.system.config_path}/.storage")
+    with open(f"{hacs.system.config_path}/.storage/hacs", "w") as h_f:
+        h_f.write("")
+    clear_storage()
+    os.makedirs(f"{hacs.system.config_path}/.storage/hacs")
+    clear_storage()
 
 
 @pytest.mark.asyncio

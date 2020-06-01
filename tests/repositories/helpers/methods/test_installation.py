f38e736b8e9eb7ed4e2897559cdb9a7f7b30b78e
ludeeus <joasoe@gmail.com>
2020-06-01 10:36:52 +0000

    add more tests

---
+++ tests/repositories/helpers/methods/test_installation.py
@@ -0,0 +1,9 @@
+import pytest
+from tests.dummy_repository import dummy_repository_integration
+
+
+@pytest.mark.asyncio
+async def test_installation(aresponses, event_loop):
+    repository = dummy_repository_integration()
+    await repository.async_pre_install()
+    await repository.async_post_install()

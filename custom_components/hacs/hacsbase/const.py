f1a2e94716b35e96c832a2ae9fa8964eae237980
Blake Niemyjski <bniemyjski@gmail.com>
2020-06-11 10:48:31 -0500

    Fixes #1324 Spelling error

--- custom_components/hacs/hacsbase/const.py
+++ custom_components/hacs/hacsbase/const.py
@@ -4,7 +4,7 @@ STORAGE_VERSION = "6"
 STORENAME = "hacs"
 
 # Messages
-NOT_SUPPORTED_HA_VERSION = "You have version '{}' of Home Assistant, but version '{}' of '{}' require version '{}' of Home Assistant, install and upgrades are disabled for this integration untill you upgrade Home Assistant."
+NOT_SUPPORTED_HA_VERSION = "You have version '{}' of Home Assistant, but version '{}' of '{}' require version '{}' of Home Assistant, install and upgrades are disabled for this integration until you upgrade Home Assistant."
 
 
 NO_ELEMENTS = "No elements to show, open the store to install some awesome stuff."

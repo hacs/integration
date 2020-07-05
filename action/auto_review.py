2ce0af40437b1703e09dc625f9840b7c2f579702
ludeeus <ludeeus@ludeeus.dev>
2020-07-05 10:44:28 +0200

    update

--- action/auto_review.py
+++ action/auto_review.py
@@ -1,6 +1,7 @@
 from astpath import search
 
 checks = set()
+exitinformation = {"code": 0}
 
 
 def add(f):
@@ -12,16 +13,22 @@ def find(string):
     return search(".", string, print_matches=False)
 
 
-def log(severity, filename, line, message):
-    print(f"::{severity} file={filename[2:]},line={line}:: {message}")
+def formatOutput(filename, line, message, error=True):
+    if error:
+        exitinformation["code"] = 1
+    print(
+        f"::{'error' if error else 'warning'} file={filename[2:]},line={line}:: {message}"
+    )
 
 
 @add
 def findForLoopsContainingAwait():
     message = "Do not await in a for loop, use asyncio.gather"
     for line in find("//AsyncFunctionDef[body//For/body//Await]"):
-        log("error", line[0], line[1], message)
+        formatOutput(line[0], line[1], message)
 
 
 for check in checks:
     check()
+
+exit(exitinformation["code"])

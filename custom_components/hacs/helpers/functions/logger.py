0101cad408300e503f382d40a8821e8a19c9dd7c
ludeeus <ludeeus@ludeeus.dev>
2020-07-06 21:04:33 +0200

    update

--- custom_components/hacs/helpers/functions/logger.py
+++ custom_components/hacs/helpers/functions/logger.py
@@ -3,16 +3,19 @@ import os
 import logging
 
 
-class HacsLogger:
-    def __init__(self, name=None) -> None:
-        self.name = f"custom_components.hacs{'.' + name if name else ''}"
-
-
 def getLogger(name=None):
-    if "GITHUB_ACTION" in os.environ or "DEVCONTAINER" in os.environ:
+    if "GITHUB_ACTION" in os.environ:
         logging.basicConfig(
             format="::%(levelname)s:: %(message)s", level="DEBUG",
         )
+    elif "DEVCONTAINER" in os.environ:
+        import colorlog
+
+        colorlog.basicConfig(
+            level="DEBUG",
+            format=f"%(log_color)s[%(levelname)s - %(name)s] - %(message)s%(reset)s",
+        )
+        return colorlog.getLogger(name)
 
     return logging.getLogger(
         f"custom_components.hacs{'.' + name if name is not None else ''}"

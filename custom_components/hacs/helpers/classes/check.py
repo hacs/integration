98d34057141c544872f213926b4ab517846deb4d
ludeeus <ludeeus@ludeeus.dev>
2020-07-08 16:52:40 +0200

    More verbose logging for action

--- custom_components/hacs/helpers/classes/check.py
+++ custom_components/hacs/helpers/classes/check.py
@@ -3,6 +3,8 @@ from abc import ABC
 from custom_components.hacs.helpers.functions.logger import getLogger
 from custom_components.hacs.share import get_hacs
 
+ACTION = "GITHUB_ACTION" in os.environ
+
 
 class RepositoryCheckException(Exception):
     pass
@@ -17,6 +19,8 @@ class RepositoryCheck(ABC):
 
     async def _async_run_check(self):
         """DO NOT OVERRIDE THIS IN SUBCLASSES!"""
+        if ACTION:
+            self.logger.info(f"Running {self.__class__.__name__}")
         try:
             await self.hacs.hass.async_add_executor_job(self.check)
             await self.async_check()

d158a3bcd3e0ef7caa08e81ad45987c5bb36f793
ludeeus <ludeeus@ludeeus.dev>
2020-07-12 09:13:55 +0000

    fix share

--- tests/test_share.py
+++ tests/test_share.py
@@ -14,6 +14,9 @@ def test_list_removed_repositories():
 def test_get_hacs():
     SHARE["hacs"] = None
     os.environ["GITHUB_ACTION"] = "value"
+    if "PYTEST" in os.environ:
+        del os.environ["PYTEST"]
     get_hacs()
     SHARE["hacs"] = None
     del os.environ["GITHUB_ACTION"]
+    os.environ["PYTEST"] = "value"

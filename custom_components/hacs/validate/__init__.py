ca0d0edb670d4c30e438e95f6994c81d202dbfef
AridRayne <aridrayne@gmail.com>
2020-08-21 08:22:02 -0700

    Remove unnecessary space.
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>

--- custom_components/hacs/validate/__init__.py
+++ custom_components/hacs/validate/__init__.py
@@ -9,7 +9,7 @@ from custom_components.hacs.share import SHARE, get_hacs
 def _initialize_rules():
     rules = glob.glob(join(dirname(__file__), "**/*.py"))
     for rule in rules:
-        rule = rule.replace (sep, "/")
+        rule = rule.replace(sep, "/")
         rule = rule.split("custom_components/hacs")[-1]
         rule = f"custom_components/hacs{rule}".replace("/", ".")[:-3]
         importlib.import_module(rule)

d2803749858d27c5ca8d6b5f2049d4445ae9b5ec
Kendell R <KTibow@users.noreply.github.com>
2020-09-13 09:38:56 -0700

    Switch description to topics (#1524)

--- custom_components/hacs/validate/common/repository_topics.py
+++ custom_components/hacs/validate/common/repository_topics.py
@@ -7,4 +7,4 @@ from custom_components.hacs.validate.base import (
 class RepositoryTopics(ActionValidationBase):
     def check(self):
         if not self.repository.data.topics:
-            raise ValidationException("The repository has no description")
+            raise ValidationException("The repository has no topics")

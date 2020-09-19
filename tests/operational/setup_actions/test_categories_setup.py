229aaa440d1ec0ae9052b360c32dc5fd3ad657a0
Ludeeus <ludeeus@ludeeus.dev>
2020-09-19 15:00:14 +0000

    Add category test

--- tests/operational/setup_actions/test_categories_setup.py
+++ tests/operational/setup_actions/test_categories_setup.py
@@ -1,3 +1,4 @@
+from custom_components.hacs.enums import HacsCategory
 import pytest
 
 from custom_components.hacs.operational.setup_actions.categories import (
@@ -7,22 +8,35 @@ from custom_components.hacs.operational.setup_actions.categories import (
 
 @pytest.mark.asyncio
 async def test_extra_stores_python_script(hacs):
-
     await async_setup_extra_stores()
-
-    assert "python_script" not in hacs.common.categories
+    assert HacsCategory.PYTHON_SCRIPT not in hacs.common.categories
     hacs.hass.config.components.add("python_script")
     await async_setup_extra_stores()
-    assert "python_script" in hacs.common.categories
-
-    hacs.hass.services._services["frontend"] = {"reload_themes": "dummy"}
+    assert HacsCategory.PYTHON_SCRIPT in hacs.common.categories
 
 
 @pytest.mark.asyncio
 async def test_extra_stores_theme(hacs):
     await async_setup_extra_stores()
-
-    assert "theme" not in hacs.common.categories
+    assert HacsCategory.THEME not in hacs.common.categories
     hacs.hass.services._services["frontend"] = {"reload_themes": "dummy"}
     await async_setup_extra_stores()
-    assert "theme" in hacs.common.categories
+    assert HacsCategory.THEME in hacs.common.categories
+
+
+@pytest.mark.asyncio
+async def test_extra_stores_appdaemon(hacs):
+    await async_setup_extra_stores()
+    assert HacsCategory.APPDAEMON not in hacs.common.categories
+    hacs.configuration.appdaemon = True
+    await async_setup_extra_stores()
+    assert HacsCategory.APPDAEMON in hacs.common.categories
+
+
+@pytest.mark.asyncio
+async def test_extra_stores_netdaemon(hacs):
+    await async_setup_extra_stores()
+    assert HacsCategory.NETDAEMON not in hacs.common.categories
+    hacs.configuration.netdaemon = True
+    await async_setup_extra_stores()
+    assert HacsCategory.NETDAEMON in hacs.common.categories

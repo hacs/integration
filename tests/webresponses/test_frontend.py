4a83641e17bbd0b74630dabbd21b47d805a19658
Ludeeus <ludeeus@ludeeus.dev>
2020-09-24 18:19:39 +0000

    fix test

--- tests/webresponses/test_frontend.py
+++ tests/webresponses/test_frontend.py
@@ -7,13 +7,13 @@ from custom_components.hacs.webresponses.frontend import async_serve_frontend
 
 @pytest.mark.asyncio
 async def test_frontend(hacs):
-    await async_serve_frontend()
+    await async_serve_frontend("dummy")
 
 
 @pytest.mark.asyncio
 async def test_frontend_debug(hacs):
     hacs.configuration.debug = True
-    await async_serve_frontend()
+    await async_serve_frontend("dummy")
     hacs.configuration = Configuration()
 
 
@@ -21,13 +21,13 @@ async def test_frontend_debug(hacs):
 async def test_frontend_repo(hacs, tmpdir):
     hacs.configuration.frontend_repo = tmpdir
 
-    await async_serve_frontend()
+    await async_serve_frontend("dummy")
 
     os.makedirs(f"{tmpdir}/hacs_frontend", exist_ok=True)
     with open(f"{tmpdir}/hacs_frontend/main.js", "w") as target:
         target.write("")
 
-    await async_serve_frontend()
+    await async_serve_frontend("dummy")
 
     hacs.configuration = Configuration()
 
@@ -41,4 +41,4 @@ async def test_frontend_frontend_repo_url(hacs, aresponses):
         "",
     )
     hacs.configuration.frontend_repo_url = "http://127.0.0.1"
-    await async_serve_frontend()
+    await async_serve_frontend("dummy")

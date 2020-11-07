b482edfb6539074b0a36e94cceb8a129e6ab2cde
Ludeeus <ludeeus@ludeeus.dev>
2020-11-07 12:57:16 +0000

    Add test to verify the issue

---
+++ tests/helpers/classes/test_repository_data.py
@@ -0,0 +1,9 @@
+from custom_components.hacs.helpers.classes.repositorydata import RepositoryData
+
+
+def test_guarded():
+    data = RepositoryData.create_from_dict({"full_name": "test"})
+    assert data.name == "test"
+
+    data.update_data({"name": "new"})
+    assert data.name == "test"

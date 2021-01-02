b2f5735e8bd5c442c4bc3dd99625df77cb42dab7
Kendell R <KTibow@users.noreply.github.com>
2021-01-02 07:24:41 -0800

    Update exceptions.py

--- custom_components/hacs/helpers/classes/exceptions.py
+++ custom_components/hacs/helpers/classes/exceptions.py
@@ -5,5 +5,9 @@ class HacsException(Exception):
     """Super basic."""
 
 
+class HacsRepositoryArchivedException(HacsException):
+    """For repos that are archived (so they raise warnings instead of errors)."""
+
+
 class HacsExpectedException(HacsException):
     """For stuff that are expected."""

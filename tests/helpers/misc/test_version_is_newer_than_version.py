5e241679ddc01d2ea9f42238ec7596bf98f1184a
Joakim Sørensen <joasoe@gmail.com>
2021-01-30 16:24:26 +0100

    Add version key to manifest file (#1851)
    * Add version key to manifest file
    
    * fix
    
    * script it
    
    * write it
    
    * downgrade
    
    * fix it
    
    * only for release
    
    * Remove wrong test
    
    * Add version

--- tests/helpers/misc/test_version_is_newer_than_version.py
+++ tests/helpers/misc/test_version_is_newer_than_version.py
@@ -25,5 +25,4 @@ def test_wierd_stuff():
     assert version_left_higher_then_right("1.0.0", "1.0.0a0")
     assert version_left_higher_then_right("1.0.0", "1.0.0b0")
     assert version_left_higher_then_right("1.0.0", "1.0.0rc0")
-    assert not version_left_higher_then_right(1.0, "1.0.0rc0")
     assert not version_left_higher_then_right("0", "1.0.0rc0")

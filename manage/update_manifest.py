2dad9e7b5034b2282941b4e3663cdccf07befe90
Ludeeus <ludeeus@ludeeus.dev>
2021-01-30 13:53:59 +0000

    write it

--- manage/update_manifest.py
+++ manage/update_manifest.py
@@ -10,11 +10,11 @@ def update_manifest():
     for index, value in enumerate(sys.argv):
         if value in ["--version", "-V"]:
             version = sys.argv[index + 1]
-    with open(f"{os.getcwd()}/custom_components/hacs/manifest.json") as manifest:
-        manifest = json.load(manifest)
 
-    manifest["version"] = version
-    print(json.dumps(manifest, indent=4, sort_keys=True))
+    with open(f"{os.getcwd()}/custom_components/hacs/manifest.json", "w") as manifest:
+        manifest = json.load(manifest)
+        manifest["version"] = version
+        manifest.write(json.dumps(manifest, indent=4, sort_keys=True))
 
 
 update_manifest()

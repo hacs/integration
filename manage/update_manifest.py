994eb5aef16364df8cdc9637322724d6eb7899c1
Ludeeus <ludeeus@ludeeus.dev>
2021-01-30 13:51:37 +0000

    script it

---
+++ manage/update_manifest.py
@@ -0,0 +1,20 @@
+"""Update the manifest file."""
+import sys
+import json
+import os
+
+
+def update_manifest():
+    """Update the manifest file."""
+    version = "0.0.0"
+    for index, value in enumerate(sys.argv):
+        if value in ["--version", "-V"]:
+            version = sys.argv[index + 1]
+    with open(f"{os.getcwd()}/custom_components/hacs/manifest.json") as manifest:
+        manifest = json.load(manifest)
+
+    manifest["version"] = version
+    print(json.dumps(manifest, indent=4, sort_keys=True))
+
+
+update_manifest()

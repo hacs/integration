a7f20f6dde33b72d7aee1ee53949a968966e588f
Ludeeus <ludeeus@ludeeus.dev>
2021-02-07 17:40:54 +0000

    Use error for disabled reason

--- custom_components/hacs/base.py
+++ custom_components/hacs/base.py
@@ -117,7 +117,7 @@ class HacsBase(HacsBaseAttributes):
         """Disable HACS."""
         self.system.disabled = True
         self.system.disabled_reason = reason
-        self.log.info("HACS is disabled - %s", reason)
+        self.log.error("HACS is disabled - %s", reason)
 
     def enable(self) -> None:
         """Enable HACS."""

ab8332f76f8bd945e5da4a7563c13cdcd284f52d
Ludeeus <ludeeus@ludeeus.dev>
2021-08-27 14:56:26 +0000

    Store data on homeassistant shutdown

---
+++ custom_components/hacs/tasks/store_hacs_data.py
@@ -0,0 +1,20 @@
+""""Store HACS data."""
+from homeassistant.const import EVENT_HOMEASSISTANT_FINAL_WRITE
+from .base import HacsTaskEventBase
+
+
+async def async_setup() -> None:
+    """Set up this task."""
+    return Task()
+
+
+class Task(HacsTaskEventBase):
+    """ "Hacs task base."""
+
+    events = [EVENT_HOMEASSISTANT_FINAL_WRITE]
+
+    async def async_execute(self) -> None:
+        if self.hacs.system.disabled:
+            return
+
+        await self.hacs.data.async_write()

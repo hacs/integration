4640a276fbb9953b94f34b414fbc33aed2c17c69
Joakim Sørensen <joasoe@gmail.com>
2021-08-29 14:28:32 +0200

    Set minimum HA version during release flow (#2191)

--- custom_components/hacs/const.py
+++ custom_components/hacs/const.py
@@ -5,7 +5,7 @@ NAME_LONG = "HACS (Home Assistant Community Store)"
 NAME_SHORT = "HACS"
 DOMAIN = "hacs"
 CLIENT_ID = "395a8e669c5de9f7c6e8"
-MINIMUM_HA_VERSION = "2021.2.0"
+MINIMUM_HA_VERSION = "0.0.0"
 PROJECT_URL = "https://github.com/hacs/integration/"
 
 

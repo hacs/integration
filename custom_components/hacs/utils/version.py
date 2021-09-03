cb99ad3fa44ece86fed3e85c90e147905616cf00
Ludeeus <ludeeus@ludeeus.dev>
2021-09-03 08:19:22 +0000

    Guard invalid versions

--- custom_components/hacs/utils/version.py
+++ custom_components/hacs/utils/version.py
@@ -3,10 +3,13 @@
 
 from functools import lru_cache
 
-from awesomeversion import AwesomeVersion
+from awesomeversion import AwesomeVersionException, AwesomeVersion
 
 
 @lru_cache(maxsize=1024)
 def version_left_higher_then_right(left: str, right: str) -> bool:
     """Return a bool if source is newer than target, will also be true if identical."""
-    return AwesomeVersion(left) >= AwesomeVersion(right)
+    try:
+        return AwesomeVersion(left) >= AwesomeVersion(right)
+    except AwesomeVersionException:
+        return False

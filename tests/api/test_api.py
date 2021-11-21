fbeeea2099ff61dd3a8aa9e5c50813ff1e990d69
Joakim Sørensen <joasoe@gmail.com>
2021-11-21 17:20:24 +0100

    Move websocket setup to task (#2304)

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -3,28 +3,23 @@ import os
 from homeassistant.core import HomeAssistant
 import pytest
 
-from custom_components.hacs.api.acknowledge_critical_repository import (
+from custom_components.hacs.tasks.setup_websocket_api import (
     acknowledge_critical_repository,
-)
-from custom_components.hacs.api.check_local_path import check_local_path
-from custom_components.hacs.api.get_critical_repositories import (
     get_critical_repositories,
+    hacs_config,
+    hacs_removed,
+    hacs_repositories,
+    hacs_repository,
+    hacs_repository_data,
+    hacs_settings,
+    hacs_status,
 )
-from custom_components.hacs.api.hacs_config import hacs_config
-from custom_components.hacs.api.hacs_removed import hacs_removed
-from custom_components.hacs.api.hacs_repositories import hacs_repositories
-from custom_components.hacs.api.hacs_repository import hacs_repository
-from custom_components.hacs.api.hacs_repository_data import hacs_repository_data
-from custom_components.hacs.api.hacs_settings import hacs_settings
-from custom_components.hacs.api.hacs_status import hacs_status
 
 
 @pytest.mark.asyncio
 async def test_check_local_path(hacs, connection, tmpdir):
     hacs.hass = HomeAssistant()
     os.makedirs(tmpdir, exist_ok=True)
-    check_local_path(hacs.hass, connection, {"path": tmpdir, "id": 1})
-    check_local_path(hacs.hass, connection, {"id": 1})
     get_critical_repositories(hacs.hass, connection, {"id": 1})
     hacs_config(hacs.hass, connection, {"id": 1})
     hacs_removed(hacs.hass, connection, {"id": 1})

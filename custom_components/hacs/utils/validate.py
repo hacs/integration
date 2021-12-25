9f2ec8c2e2dffc18fe2b8001bf9be69da3626e07
Ludeeus <ludeeus@ludeeus.dev>
2021-12-25 14:31:23 +0000

    Add typehint

--- custom_components/hacs/utils/validate.py
+++ custom_components/hacs/utils/validate.py
@@ -8,6 +8,6 @@ class Validate:
     errors: list[str] = field(default_factory=list)
 
     @property
-    def success(self):
+    def success(self) -> bool:
         """Return bool if the validation was a success."""
         return len(self.errors) == 0

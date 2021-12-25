8b3e23f4ea3dbbd8bacbdcd404fb67a0dcab3872
Joakim Sørensen <joasoe@gmail.com>
2021-12-25 18:41:59 +0100

    Remove helpers.misc (#2376)

---
+++ custom_components/hacs/utils/regex.py
@@ -0,0 +1,15 @@
+"""Regex utils"""
+from __future__ import annotations
+import re
+
+RE_REPOSITORY = re.compile(
+    r"(?:(?:.*github.com.)|^)([A-Za-z0-9-]+\/[\w.-]+?)(?:(?:\.git)?|(?:[^\w.-].*)?)$"
+)
+
+
+def extract_repository_from_url(url: str) -> str | None:
+    """Extract the owner/repo part form a URL."""
+    match = re.match(RE_REPOSITORY, url)
+    if not match:
+        return None
+    return match.group(1).lower()
--- tests/helpers/functions/test_extract_repository_from_url.py
+++ tests/helpers/functions/test_extract_repository_from_url.py
@@ -1,14 +1,14 @@
 """Tests for repository extraction."""
-from custom_components.hacs.helpers.functions.misc import extract_repository_from_url
+from custom_components.hacs.utils import regex
 
 
 def test_extract_repository_from_url():
     """Tests for repository extraction."""
-    assert extract_repository_from_url("https://github.com/user/repo") == "user/repo"
-    assert extract_repository_from_url("user/repo/") == "user/repo"
-    assert extract_repository_from_url("user/repo") == "user/repo"
-    assert extract_repository_from_url("USER/REPO") == "user/repo"
-    assert extract_repository_from_url("user/repo.git") == "user/repo"
-    assert extract_repository_from_url("user/repo.repo") == "user/repo.repo"
-    assert extract_repository_from_url("git@github.com:user/repo.git") == "user/repo"
-    assert not extract_repository_from_url("https://google.com/user/repo")
+    assert regex.extract_repository_from_url("https://github.com/user/repo") == "user/repo"
+    assert regex.extract_repository_from_url("user/repo/") == "user/repo"
+    assert regex.extract_repository_from_url("user/repo") == "user/repo"
+    assert regex.extract_repository_from_url("USER/REPO") == "user/repo"
+    assert regex.extract_repository_from_url("user/repo.git") == "user/repo"
+    assert regex.extract_repository_from_url("user/repo.repo") == "user/repo.repo"
+    assert regex.extract_repository_from_url("git@github.com:user/repo.git") == "user/repo"
+    assert not regex.extract_repository_from_url("https://google.com/user/repo")

41e9e21ab8a5028540049a908bddb81b4b45fca8
Ludeeus <ludeeus@ludeeus.dev>
2021-12-30 20:14:39 +0000

    Add exception_handler to handle common exceptions

---
+++ tests/test_exception_handler.py
@@ -0,0 +1,62 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring
+from aiogithubapi import (
+    GitHubAuthenticationException,
+    GitHubNotModifiedException,
+    GitHubRatelimitException,
+)
+from aiogithubapi.exceptions import GitHubException
+import pytest
+
+from custom_components.hacs.base import HacsBase
+from custom_components.hacs.enums import HacsDisabledReason
+from custom_components.hacs.exceptions import HacsException
+
+
+def test_exception_handler_githubauthenticationexception(hacs: HacsBase, caplog: pytest.LogCaptureFixture):
+    assert not hacs.system.disabled
+
+    try:
+        raise GitHubAuthenticationException()
+    except BaseException as exception:
+        hacs.exception_handler(exception)
+
+    assert hacs.system.disabled
+    assert hacs.system.disabled_reason == HacsDisabledReason.INVALID_TOKEN
+    assert "GitHub authentication failed" in caplog.text
+
+
+def test_exception_handler_githubratelimitexception(hacs: HacsBase, caplog: pytest.LogCaptureFixture):
+    assert not hacs.system.disabled
+
+    try:
+        raise GitHubRatelimitException()
+    except BaseException as exception:
+        hacs.exception_handler(exception)
+
+    assert hacs.system.disabled
+    assert hacs.system.disabled_reason == HacsDisabledReason.RATE_LIMIT
+    assert "GitHub API ratelimited" in caplog.text
+
+
+def test_exception_handler_githubnotmodifiedexception(hacs: HacsBase):
+    try:
+        raise GitHubNotModifiedException()
+    except BaseException as exception:
+        with pytest.raises(GitHubNotModifiedException):
+            hacs.exception_handler(exception)
+
+def test_exception_handler_githubexception(hacs: HacsBase, caplog: pytest.LogCaptureFixture):
+    try:
+        raise GitHubException()
+    except BaseException as exception:
+        with pytest.raises(HacsException):
+            hacs.exception_handler(exception)
+            assert "GitHub API error" in caplog.text
+
+
+def test_exception_handler_baseexception(hacs: HacsBase):
+    try:
+        raise BaseException()
+    except BaseException as exception:
+        with pytest.raises(HacsException):
+            hacs.exception_handler(exception)
--- tests/test_globals.py
+++
@@ -1,8 +0,0 @@
-"""Test globals."""
-# pylint: disable=missing-docstring
-
-
-def test_global_hacs(hacs):
-    assert hacs.core.lovelace_mode == "yaml"
-    hacs.core.lovelace_mode = "storage"
-    assert hacs.core.lovelace_mode == "storage"

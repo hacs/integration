998ca45ecc2e8fdc1a50e2f8894c53468d81b98d
Ludeeus <ludeeus@ludeeus.dev>
2022-02-05 11:17:08 +0000

    Add tests for update_downloaded_repositories

--- manage/update_default_repositories.py
+++ manage/update_default_repositories.py
@@ -6,10 +6,16 @@ import sys
 
 def update():
     """Update the shipped default repositories data file."""
-    storage, to_store = None, {}
+    storage, to_store, old = None, {}, {}
+    updated = 0
 
     with open(f"{os.getcwd()}/config/.storage/hacs.repositories", encoding="utf-8") as storage_file:
         storage = json.load(storage_file)
+
+    with open(
+        f"{os.getcwd()}/custom_components/hacs/utils/default.repositories", encoding="utf-8"
+    ) as old_file:
+        old = json.load(old_file)
 
     if storage is None:
         sys.exit("No storage file")
@@ -21,6 +27,9 @@ def update():
         for key in ("installed_commit", "selected_tag", "version_installed"):
             storage["data"][repo][key] = None
 
+        if old.get(repo, {}).get("etag_repository") != storage["data"][repo].get("etag_repository"):
+            updated += 1
+
         to_store[repo] = storage["data"][repo]
 
     with open(
@@ -29,6 +38,8 @@ def update():
         encoding="utf-8",
     ) as to_store_file:
         to_store_file.write(json.dumps(to_store))
+
+    print(f"{updated} was updated")
 
 
 if __name__ == "__main__":

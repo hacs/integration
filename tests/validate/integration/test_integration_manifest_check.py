334aead3d1e162a4511b5d022b2777d5a3a650ca
Ludeeus <ludeeus@ludeeus.dev>
2022-05-24 21:26:32 +0000

    Use voluptuous to validate keys in manifest.json that HACS require

--- tests/validate/integration/test_integration_manifest_check.py
+++ tests/validate/integration/test_integration_manifest_check.py
@@ -12,12 +12,42 @@ async def test_hacs_manifest_no_manifest(repository_integration):
 
 
 @pytest.mark.asyncio
-async def test_hacs_manifest_with_manifest(repository_integration):
+async def test_hacs_manifest_with_valid_manifest(repository_integration):
     repository_integration.tree = [
         AIOGitHubAPIRepositoryTreeContent(
             {"path": "manifest.json", "type": "file"}, "test/test", "main"
         )
     ]
+
+    async def _async_get_integration_manifest(_):
+        return {
+            "domain": "test",
+            "documentation": "https://hacs.xyz",
+            "issue_tracker": "https://hacs.xyz",
+            "codeowners": ["test"],
+            "name": "test",
+            "version": "1.0.0",
+        }
+
+    repository_integration.async_get_integration_manifest = _async_get_integration_manifest
+
     check = Validator(repository_integration)
     await check.execute_validation()
     assert not check.failed
+
+
+@pytest.mark.asyncio
+async def test_hacs_manifest_with_invalid_manifest(repository_integration):
+    repository_integration.tree = [
+        AIOGitHubAPIRepositoryTreeContent(
+            {"path": "manifest.json", "type": "file"}, "test/test", "main"
+        )
+    ]
+
+    async def _async_get_integration_manifest(_):
+        return {"not": "valid"}
+
+    repository_integration.async_get_integration_manifest = _async_get_integration_manifest
+    check = Validator(repository_integration)
+    await check.execute_validation()
+    assert check.failed

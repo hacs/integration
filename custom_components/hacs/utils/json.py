91b45221208ae89bcc2d5018947f160b90f38924
ludeeus <ludeeus@ludeeus.dev>
2022-07-03 18:06:53 +0000

    Add comment

--- custom_components/hacs/utils/json.py
+++ custom_components/hacs/utils/json.py
@@ -1,6 +1,8 @@
 """JSON utils."""
 
 try:
+    # Could be removed after 2022.06 is the min version
+    # But in case Home Assistant changes, keep this try/except here...
     from homeassistant.helpers.json import json_loads
 except ImportError:
     from json import loads as json_loads

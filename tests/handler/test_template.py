d03ec9c66cb85bd1ae0e4cb07fd33f4666524456
ludeeus <ludeeus@ludeeus.dev>
2022-07-04 14:01:36 +0000

    Do not render template for experimental

--- tests/handler/test_template.py
+++ tests/handler/test_template.py
@@ -9,10 +9,14 @@ class MockRelease:
     prerelease = True
 
 
-def test_render_template(repository):
+def test_render_template(hacs, repository):
     content = "ABC"
-    render_template(content, repository)
+    render_template(hacs, content, repository)
     repository.releases.last_release_object = MockRelease()
-    render_template(content, repository)
+    render_template(hacs, content, repository)
 
-    assert render_template("{{test.test}}", repository) == "{{test.test}}"
+    assert render_template(hacs, "{{test.test}}", repository) == "{{test.test}}"
+    assert render_template(hacs, "{%if True%}hi{%endif%}", repository) == "hi"
+
+    hacs.configuration.experimental = True
+    assert render_template(hacs, "{%if True%}hi{%endif%}", repository) == "{%if True%}hi{%endif%}"

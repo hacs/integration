6dc933dac1461cff1ae251009d0c043abd974883
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-08-09 22:50:49 +0200

    Add repair issue for removed repositories (#2823)
    * Add repair issue for removed repositories
    
    * Update custom_components/hacs/translations/en.json

--- custom_components/hacs/repositories/base.py
+++ custom_components/hacs/repositories/base.py
@@ -19,6 +19,7 @@ from aiogithubapi.const import BASE_API_URL
 from aiogithubapi.objects.repository import AIOGitHubAPIRepository
 import attr
 from homeassistant.helpers import device_registry as dr
+from homeassistant.components.repairs import async_delete_issue
 
 from ..const import DOMAIN
 from ..enums import ConfigurationType, HacsDispatchEvent, RepositoryFile
@@ -734,6 +735,7 @@ class HacsRepository:
         )
 
         await self.async_remove_entity_device()
+        async_delete_issue(self.hacs.hass, DOMAIN, f"removed_{self.data.id}")
 
     async def remove_local_directory(self) -> None:
         """Check the local directory."""

49f47e3dc6f42ec66c725b241b0e19a156ad5293
Joakim Sørensen <ludeeus@ludeeus.dev>
2023-01-14 09:39:35 +0100

    Add etag support for data client (#2997)

--- custom_components/hacs/data_client.py
+++ custom_components/hacs/data_client.py
@@ -5,7 +5,7 @@ from typing import Any
 
 from aiohttp import ClientSession, ClientTimeout
 
-from .exceptions import HacsException
+from .exceptions import HacsException, HacsNotModifiedException
 
 
 class HacsDataClient:
@@ -14,6 +14,7 @@ class HacsDataClient:
     def __init__(self, session: ClientSession, client_name: str) -> None:
         """Initialize."""
         self._client_name = client_name
+        self._etags = {}
         self._session = session
 
     async def _do_request(
@@ -27,11 +28,20 @@ class HacsDataClient:
             response = await self._session.get(
                 f"https://data-v2.hacs.xyz/{endpoint}",
                 timeout=ClientTimeout(total=60),
-                headers={"User-Agent": self._client_name},
+                headers={
+                    "User-Agent": self._client_name,
+                    "If-None-Match": self._etags.get(endpoint, ""),
+                },
             )
+            if response.status == 304:
+                raise HacsNotModifiedException() from None
             response.raise_for_status()
+        except HacsNotModifiedException:
+            raise
         except Exception as exception:
             raise HacsException(f"Error fetching data from HACS: {exception}") from exception
+
+        self._etags[endpoint] = response.headers.get("etag")
 
         return await response.json()
 

a8205ce1ecc8435af4a1dca7cc4671af252a7152
Joakim Sørensen <joasoe@gmail.com>
2023-07-08 09:53:33 +0200

    Revert "Better cleanup of scheduled tasks (#3150)"
    This reverts commit 8219c8161c20b59ab133036046f995a07ff19785.

--- custom_components/hacs/utils/queue_manager.py
+++ custom_components/hacs/utils/queue_manager.py
@@ -5,27 +5,26 @@ import asyncio
 import time
 from typing import Coroutine
 
+from homeassistant.core import HomeAssistant
+
 from ..exceptions import HacsExecutionStillInProgress
 from .logger import LOGGER
+
+_LOGGER = LOGGER
 
 
 class QueueManager:
     """The QueueManager class."""
 
-    def __init__(self) -> None:
-        self._queue: list[asyncio.Task] = []
-        self._execution_group: asyncio.Future | None = None
-        self._stopping = False
-
-    @property
-    def running(self) -> bool:
-        """Return a bool indicating if we are already running."""
-        return self._execution_group is not None
+    def __init__(self, hass: HomeAssistant) -> None:
+        self.hass = hass
+        self.queue: list[Coroutine] = []
+        self.running = False
 
     @property
     def pending_tasks(self) -> int:
         """Return a count of pending tasks in the queue."""
-        return len(self._queue)
+        return len(self.queue)
 
     @property
     def has_pending_tasks(self) -> bool:
@@ -34,58 +33,49 @@ class QueueManager:
 
     def clear(self) -> None:
         """Clear the queue."""
-        self._stopping = True
-        for task in self._queue:
-            task.cancel()
-        if self._execution_group is not None:
-            self._execution_group.cancel()
-            self._execution_group = None
-        self._queue = []
-        self._stopping = False
+        self.queue = []
 
     def add(self, task: Coroutine) -> None:
         """Add a task to the queue."""
-        _task = asyncio.create_task(task)
-        if self._stopping:
-            _task.cancel()
-            return
-        self._queue.append(_task)
+        self.queue.append(task)
 
     async def execute(self, number_of_tasks: int | None = None) -> None:
         """Execute the tasks in the queue."""
         if self.running:
-            LOGGER.debug("<QueueManager> Execution is already running")
+            _LOGGER.debug("<QueueManager> Execution is already running")
             raise HacsExecutionStillInProgress
-        if self.pending_tasks == 0:
-            LOGGER.debug("<QueueManager> The queue is empty")
-            return
-        if self._stopping:
-            LOGGER.debug("<QueueManager> The queue is stopping")
+        if len(self.queue) == 0:
+            _LOGGER.debug("<QueueManager> The queue is empty")
             return
 
-        LOGGER.debug("<QueueManager> Checking out tasks to execute")
-        local_queue: list[asyncio.Task] = []
+        self.running = True
 
-        for task in self._queue[:number_of_tasks]:
-            local_queue.append(task)
-            self._queue.remove(task)
+        _LOGGER.debug("<QueueManager> Checking out tasks to execute")
+        local_queue = []
 
-        local_queue_count = len(local_queue)
+        if number_of_tasks:
+            for task in self.queue[:number_of_tasks]:
+                local_queue.append(task)
+        else:
+            for task in self.queue:
+                local_queue.append(task)
 
-        LOGGER.debug("<QueueManager> Starting queue execution for %s tasks", local_queue_count)
+        for task in local_queue:
+            self.queue.remove(task)
+
+        _LOGGER.debug("<QueueManager> Starting queue execution for %s tasks", len(local_queue))
         start = time.time()
-        self._execution_group = asyncio.gather(*local_queue, return_exceptions=True)
-        result = await self._execution_group
+        result = await asyncio.gather(*local_queue, return_exceptions=True)
         for entry in result:
             if isinstance(entry, Exception):
-                LOGGER.error("<QueueManager> %s", entry)
+                _LOGGER.error("<QueueManager> %s", entry)
         end = time.time() - start
 
-        LOGGER.debug(
+        _LOGGER.debug(
             "<QueueManager> Queue execution finished for %s tasks finished in %.2f seconds",
-            local_queue_count,
+            len(local_queue),
             end,
         )
         if self.has_pending_tasks:
-            LOGGER.debug("<QueueManager> %s tasks remaining in the queue", self.pending_tasks)
-        self._execution_group = None
+            _LOGGER.debug("<QueueManager> %s tasks remaining in the queue", len(self.queue))
+        self.running = False

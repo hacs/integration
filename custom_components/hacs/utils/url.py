e4df9e86cf28f2c98e02912d046b9e9e16e964d5
ludeeus <joasoe@gmail.com>
2023-10-13 22:10:32 +0000

    lint

--- custom_components/hacs/utils/url.py
+++ custom_components/hacs/utils/url.py
@@ -1,5 +1,6 @@
 """Various URL utils for HACS."""
 
+
 def asset_download(repository: str, version: str, filenme: str) -> str:
     """Generate a download URL for a release asset."""
     return f"https://github.com/{repository}/releases/download/{version}/{filenme}"

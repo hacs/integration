8f368311248615776c5841f50d59081c9a13ddd1
ludeeus <joasoe@gmail.com>
2023-11-19 20:40:29 +0000

    lint

--- tests/integration/test_integration_setup.py
+++ tests/integration/test_integration_setup.py
@@ -1,15 +1,21 @@
 from typing import Generator
-import pytest
+
 from homeassistant.core import HomeAssistant
-from tests.common import create_config_entry
-from custom_components.hacs.const import DOMAIN
+import pytest
+
 from custom_components.hacs.base import HacsBase
+from custom_components.hacs.const import DOMAIN
 
-
+from tests.common import create_config_entry
 from tests.conftest import SnapshotFixture
 
+
 @pytest.mark.asyncio
-async def test_integration_setup(hass: HomeAssistant, proxy_session: Generator, snapshots: SnapshotFixture,):
+async def test_integration_setup(
+    hass: HomeAssistant,
+    proxy_session: Generator,
+    snapshots: SnapshotFixture,
+):
     config_entry = create_config_entry(options={"experimental": True})
     hass.data.pop("custom_components", None)
     config_entry.add_to_hass(hass)
@@ -20,4 +26,3 @@ async def test_integration_setup(hass: HomeAssistant, proxy_session: Generator, 
     assert not hacs.system.disabled
 
     await snapshots.assert_hacs_data(hacs, f"test_integration_setup.json")
-
--- tests/validate/test_brands_check.py
+++ tests/validate/test_brands_check.py
@@ -1,12 +1,16 @@
 import pytest
 
 from custom_components.hacs.validate.brands import Validator
-from tests.common import ResponseMocker, MockedResponse
+
+from tests.common import MockedResponse, ResponseMocker
 
 
 @pytest.mark.asyncio
 async def test_added_to_brands(repository, response_mocker: ResponseMocker):
-    response_mocker.add("https://brands.home-assistant.io/domains.json", MockedResponse(content={"custom": ["test"]}))
+    response_mocker.add(
+        "https://brands.home-assistant.io/domains.json",
+        MockedResponse(content={"custom": ["test"]}),
+    )
     repository.data.domain = "test"
     check = Validator(repository)
     await check.execute_validation()
@@ -15,7 +19,9 @@ async def test_added_to_brands(repository, response_mocker: ResponseMocker):
 
 @pytest.mark.asyncio
 async def test_not_added_to_brands(repository, response_mocker: ResponseMocker):
-    response_mocker.add("https://brands.home-assistant.io/domains.json", MockedResponse(content={"custom": []}))
+    response_mocker.add(
+        "https://brands.home-assistant.io/domains.json", MockedResponse(content={"custom": []})
+    )
     repository.data.domain = "test"
     check = Validator(repository)
     await check.execute_validation()

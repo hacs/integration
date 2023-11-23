9f47488861c1a2f10c4c6bea476785acd13cae5a
ludeeus <joasoe@gmail.com>
2023-11-23 20:07:36 +0000

    Test addons and core

--- tests/repositories/test_register_repository.py
+++ tests/repositories/test_register_repository.py
@@ -1,9 +1,10 @@
 from typing import Generator
 
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 import pytest
 
-from custom_components.hacs.enums import HacsCategory
+from custom_components.hacs.enums import HacsCategory, HacsDispatchEvent
 
 from tests.common import WSClient, get_hacs, recursive_remove_key, safe_json_dumps
 from tests.conftest import SnapshotFixture
@@ -42,3 +43,60 @@ async def test_register_repository(
         safe_json_dumps(recursive_remove_key(response["result"], ("last_updated", "local_path"))),
         f"{repository_full_name}/test_register_repository.json",
     )
+
+
+@pytest.mark.parametrize(
+    "repository_full_name,result",
+    (
+        (
+            "home-assistant/core",
+            "You can not add homeassistant/core, to use core integrations check the Home Assistant documentation for how to add them.",
+        ),
+        (
+            "home-assistant/addons",
+            "The repository does not seem to be a integration, but an add-on repository. HACS does not manage add-ons.",
+        ),
+        (
+            "hassio-addons/example",
+            "The repository does not seem to be a integration, but an add-on repository. HACS does not manage add-ons.",
+        ),
+        (
+            "hacs-test-org/addon-basic",
+            "The repository does not seem to be a integration, but an add-on repository. HACS does not manage add-ons.",
+        ),
+        (
+            "hacs-test-org/integration-invalid",
+            "<Integration hacs-test-org/integration-invalid> Repository structure for main is not compliant",
+        ),
+    ),
+)
+@pytest.mark.asyncio
+async def test_register_repository_failures(
+    hass: HomeAssistant,
+    setup_integration: Generator,
+    repository_full_name: str,
+    result: str,
+    ws_client: WSClient,
+):
+    messages = []
+
+    async_dispatcher_connect(
+        hass,
+        HacsDispatchEvent.ERROR,
+        lambda message: messages.append(message),
+    )
+    hacs = get_hacs(hass)
+    assert hacs.repositories.get_by_full_name(repository_full_name) is None
+
+    response = await ws_client.send_and_receive_json(
+        "hacs/repositories/add",
+        {"repository": repository_full_name, "category": HacsCategory.INTEGRATION},
+    )
+    await hass.async_block_till_done()
+    assert response["success"] == True
+    repo = hacs.repositories.get_by_full_name(repository_full_name)
+    assert repo is None
+    assert response["result"] == {}
+    assert len(messages) == 1
+    assert messages[0]["action"] == "add_repository"
+    assert messages[0]["message"] == result

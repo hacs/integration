2bc6dcd68cb996779f3493498227aca1e0fda923
ludeeus <joasoe@gmail.com>
2024-01-13 14:07:49 +0000

    Guard async_suggest_report_issue

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,31 @@
-from homeassistant import core, loader
+import json
+from awesomeversion import AwesomeVersion
+from homeassistant import core, loader, const
 
-_orig_async_suggest_report_issue = loader.async_suggest_report_issue
+
 
 _async_suggest_report_issue_mock_call_tracker = []
+
+try:
+    _orig_async_suggest_report_issue = loader.async_suggest_report_issue
+except AttributeError:
+    if AwesomeVersion(const.__version__) >= "2023.11.0":
+        raise RuntimeError(
+            "loader.async_suggest_report_issue does not exist"
+        )
+
+    @core.callback
+    def _fallback_async_suggest_report_issue(*args, **kwargs):
+        return json.dumps(kwargs)
+
+    _orig_async_suggest_report_issue = _fallback_async_suggest_report_issue
 
 
 @core.callback
 def async_suggest_report_issue_mock(*args, **kwargs):
-    _async_suggest_report_issue_mock_call_tracker.append((args, kwargs))
-    return _orig_async_suggest_report_issue(*args, **kwargs)
+    result = _orig_async_suggest_report_issue(*args, **kwargs)
+    _async_suggest_report_issue_mock_call_tracker.append(result)
+    return result
 
 
 loader.async_suggest_report_issue = async_suggest_report_issue_mock

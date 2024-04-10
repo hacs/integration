3f09f5fc38319d7c93a23f53fb37a984d8cd9d69
Joakim Sørensen <ludeeus@ludeeus.dev>
2024-04-10 16:15:35 +0200

    Refactor testing (#3357)
    * init more tests
    
    * Update snapshots
    
    * update fixtures
    
    * sort files
    
    * Remove tests before running hassfest
    
    * fix json
    
    * more test adjustments
    
    * lint
    
    * add psutil_home_assistant test requirement
    
    * sqlalchemy
    
    * fnv_hash_fast
    
    * more
    
    * PIL
    
    * Pillow
    
    * janus
    
    * hass_frontend
    
    * home-assistant-frontend
    
    * full entry for newer
    
    * lint
    
    * get entity for update test
    
    * renames
    
    * pin test packages
    
    * single t
    
    * updates
    
    * fix stuff
    
    * store entities and ws for setup test
    
    * remove base
    
    * Add basic fixtures for all categories
    
    * Use WS command to register custom repository
    
    * lint
    
    * cleanup
    
    * unused import
    
    * Test addons and core
    
    * start generate tests
    
    * Output all calls to proxy
    
    * change messaging
    
    * lint
    
    * remove old
    
    * plugin
    
    * lint
    
    * try with minor_version first
    
    * theme
    
    * lint
    
    * Setup all categories for tests
    
    * lint
    
    * Cleanup stale
    
    * Remove unnecessary test files
    
    * Remove aresponses
    
    * remove proxy_session usage
    
    * more!
    
    * remove backup arg
    
    * fix
    
    * lint
    
    * Remove autouse from setup_integration fixture
    
    * add entities to snapshots
    
    * Fix HACS test manifest
    
    * filter calls
    
    * replace diagnostics test
    
    * Fix sorting
    
    * better output
    
    * adjust resp
    
    * ignore minor_version
    
    * replace print in test
    
    * add appdaemon
    
    * cleanup
    
    * lint
    
    * Add test for removing repositories
    
    * lint
    
    * update output
    
    * extend generate
    
    * More usage of common test data
    
    * data client test
    
    * output
    
    * add python_script tests
    
    * typo
    
    * enable all
    
    * config_flow tests
    
    * remaining generate
    
    * use xfail
    
    * lint
    
    * lint
    
    * sort
    
    * adjust
    
    * cover bad cases
    
    * one more
    
    * update snapshots
    
    * init better action testing
    
    * track loader.async_suggest_report_issue
    
    * lint
    
    * Sort action results
    
    * lint...
    
    * Guard async_suggest_report_issue
    
    * lint
    
    * Run dev tests against 3.12 as well
    
    * change names
    
    * move report check to fixture
    
    * fix version selector
    
    * Add setuptools to base
    
    * fix check_report_issue check
    
    * Use set_state to bust cached propery
    
    * Use async_test_home_assistant test helpers from homeassistant core repo
    
    * Lint
    
    * Update snapshots
    
    * Align with HA Core 2024.4.0
    
    * Fix test_update_repository_entity
    
    * Fix import
    
    * Fix test_hacs_action_integration
    
    * Disable testing dev core with Python 3.11
    
    * Allow more issues
    
    * Use Python 3.12 run in codecov
    
    * Enable config flow tests
    
    * Adjust tests
    
    * Update generate_category_data tests
    
    * Clear stored responses after usage
    
    * Rename tests/z_action to tests/action
    
    * Update snapshot
    
    * Update action test
    
    * Remove cleanups
    
    * use with expectation
    
    * fix typing
    
    * Use python version from matrix
    
    * Test option flow
    
    * Lint
    
    * Update snapshots
    
    * Use token constant
    
    ---------
    
    Co-authored-by: Erik <erik@montnemery.com>

---
+++ tests/action/test_hacs_action_integration.py
@@ -0,0 +1,73 @@
+"""Tests for the HACS action."""
+import base64
+import json
+import os
+from unittest import mock
+
+import pytest
+
+from tests.common import TOKEN, MockedResponse, ResponseMocker, current_function_name
+from tests.conftest import SnapshotFixture
+
+
+@pytest.mark.parametrize(
+    "key,manifest,succeed",
+    (
+        ("bad_documentation", {"documentation": None}, False),
+        ("bad_issue_tracker", {"issue_tracker": None}, False),
+        ("valid_manifest1", {}, True),
+    ),
+)
+async def test_hacs_action_integration(
+    key: str,
+    manifest: dict[str, str | bool | None],
+    succeed: bool,
+    caplog: pytest.LogCaptureFixture,
+    response_mocker: ResponseMocker,
+    snapshots: SnapshotFixture,
+):
+    """Test the action."""
+    basemanifest = {
+        "domain": "example",
+        "version": "1.0.0",
+        "documentation": "https://example.com",
+        "codeowners": ["hacs-test-org"],
+        "issue_tracker": "https://example.com",
+        "name": "Example",
+    }
+    envpatch = {
+        "INPUT_GITHUB_TOKEN": TOKEN,
+        "INPUT_REPOSITORY": "hacs-test-org/integration-basic",
+        "INPUT_CATEGORY": "integration",
+    }
+
+    response_mocker.add(
+        "https://brands.home-assistant.io/domains.json",
+        response=MockedResponse(status=200, content={"custom": ["example"]}),
+    )
+    response_mocker.add(
+        f"https://api.github.com/repos/hacs-test-org/integration-basic/contents/custom_components/example/manifest.json",
+        response=MockedResponse(
+            status=200,
+            content={
+                "content": base64.b64encode(
+                    json.dumps({**basemanifest, **manifest}).encode("ascii")
+                ).decode("ascii")
+            },
+            keep=True,
+        ),
+    )
+
+    with mock.patch.dict(os.environ, envpatch), mock.patch("builtins.exit", mock.MagicMock()):
+        from action.action import preflight
+
+        await preflight()
+
+    assert ("All (8) checks passed" if succeed else "1/8 checks failed") in caplog.text
+
+    splitlines = [f"<{l.rsplit(' <')[1]}" for l in caplog.text.split("\n") if " <" in l]
+
+    snapshots.assert_match(
+        "\n".join(splitlines[0:2] + sorted(splitlines[2:-2]) + splitlines[-2:]),
+        f"action/{current_function_name()}/{key}.log",
+    )

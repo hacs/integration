6c852b8f7799cb5608fac898bebc3e481d50bcba
J. Nick Koston <nick@koston.org>
2024-04-24 08:22:40 +0200

    Add a cache to is_safe
    fixes #3540

--- custom_components/hacs/utils/path.py
+++ custom_components/hacs/utils/path.py
@@ -3,19 +3,25 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import TYPE_CHECKING
+from functools import lru_cache
 
 if TYPE_CHECKING:
-    from ..base import HacsBase
+    from ..base import HacsBase, HacsConfiguration
+
+@lru_cache(maxsize=1)
+def _get_safe_paths(config_path: str, configuration: HacsConfiguration) -> set[str]:
+    """Get safe paths."""
+    return {
+        Path(f"{config_path}/{configuration.appdaemon_path}").as_posix(),
+        Path(f"{config_path}/{configuration.netdaemon_path}").as_posix(),
+        Path(f"{config_path}/{configuration.plugin_path}").as_posix(),
+        Path(f"{config_path}/{configuration.python_script_path}").as_posix(),
+        Path(f"{config_path}/{configuration.theme_path}").as_posix(),
+        Path(f"{config_path}/custom_components/").as_posix(),
+        Path(f"{config_path}/custom_templates/").as_posix(),
+    }    
 
 
 def is_safe(hacs: HacsBase, path: str | Path) -> bool:
     """Helper to check if path is safe to remove."""
-    return Path(path).as_posix() not in (
-        Path(f"{hacs.core.config_path}/{hacs.configuration.appdaemon_path}").as_posix(),
-        Path(f"{hacs.core.config_path}/{hacs.configuration.netdaemon_path}").as_posix(),
-        Path(f"{hacs.core.config_path}/{hacs.configuration.plugin_path}").as_posix(),
-        Path(f"{hacs.core.config_path}/{hacs.configuration.python_script_path}").as_posix(),
-        Path(f"{hacs.core.config_path}/{hacs.configuration.theme_path}").as_posix(),
-        Path(f"{hacs.core.config_path}/custom_components/").as_posix(),
-        Path(f"{hacs.core.config_path}/custom_templates/").as_posix(),
-    )
+    return Path(path).as_posix() not in _get_safe_paths(hacs.core.config_path, hacs.configuration)

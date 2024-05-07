b9c91cb2538eb7f5e07e02c7c1468462c1e411be
Erik <erik@montnemery.com>
2024-05-07 16:37:13 +0200

    Bump minimum HA version to 2024.4.0

--- tests/common.py
+++ tests/common.py
@@ -26,12 +26,16 @@ from homeassistant.const import (
 from homeassistant.core import CoreState, HomeAssistant, callback
 from homeassistant.helpers import (
     area_registry as ar,
+    category_registry as cr,
     device_registry as dr,
     entity,
     entity_registry as er,
+    floor_registry as fr,
     issue_registry as ir,
+    label_registry as lr,
     restore_state as rs,
     storage,
+    translation,
 )
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.json import ExtendedJSONEncoder
@@ -255,15 +259,18 @@ class StoreWithoutWriteLoad(storage.Store[_T]):
 
 
 # pylint: disable=protected-access
+@asynccontextmanager
 async def async_test_home_assistant_min_version(
-    loop, load_registries=True, config_dir: str | None = None
-):
+    event_loop: asyncio.AbstractEventLoop | None = None,
+    load_registries: bool = True,
+    config_dir: str | None = None,
+) -> AsyncGenerator[HomeAssistant, None]:
     """Return a Home Assistant object pointing at test config dir.
 
     This should be copied from the minimum supported version,
-    currently Home Assistant Core 2023.6.0.
+    currently Home Assistant Core 2024.4.0.
     """
-    hass = HomeAssistant()
+    hass = HomeAssistant(config_dir or get_test_config_dir())
     store = auth_store.AuthStore(hass)
     hass.auth = auth.AuthManager(hass, store, {}, {})
     ensure_auth_manager_loaded(hass.auth)
@@ -272,8 +279,9 @@ async def async_test_home_assistant_min_version(
     orig_async_add_job = hass.async_add_job
     orig_async_add_executor_job = hass.async_add_executor_job
     orig_async_create_task = hass.async_create_task
+    orig_tz = dt_util.DEFAULT_TIME_ZONE
 
-    def async_add_job(target, *args):
+    def async_add_job(target, *args, eager_start: bool = False):
         """Add job."""
         check_target = target
         while isinstance(check_target, ft.partial):
@@ -284,7 +292,7 @@ async def async_test_home_assistant_min_version(
             fut.set_result(target(*args))
             return fut
 
-        return orig_async_add_job(target, *args)
+        return orig_async_add_job(target, *args, eager_start=eager_start)
 
     def async_add_executor_job(target, *args):
         """Add executor job."""
@@ -299,14 +307,14 @@ async def async_test_home_assistant_min_version(
 
         return orig_async_add_executor_job(target, *args)
 
-    def async_create_task(coroutine, name=None):
+    def async_create_task(coroutine, name=None, eager_start=False):
         """Create task."""
         if isinstance(coroutine, Mock) and not isinstance(coroutine, AsyncMock):
             fut = asyncio.Future()
             fut.set_result(None)
             return fut
 
-        return orig_async_create_task(coroutine, name)
+        return orig_async_create_task(coroutine, name, eager_start)
 
     hass.async_add_job = async_add_job
     hass.async_add_executor_job = async_add_executor_job
@@ -315,16 +323,12 @@ async def async_test_home_assistant_min_version(
     hass.data[loader.DATA_CUSTOM_COMPONENTS] = {}
 
     hass.config.location_name = "test home"
-    # HACS: Modified to use the passed in config_dir
-    # hass.config.config_dir = get_test_config_dir()
-    hass.config.config_dir = config_dir
     hass.config.latitude = 32.87336
     hass.config.longitude = -117.22743
     hass.config.elevation = 0
     hass.config.set_time_zone("US/Pacific")
     hass.config.units = METRIC_SYSTEM
-    # HACS: Disabled
-    # hass.config.media_dirs = {"local": get_test_config_dir("media")}
+    hass.config.media_dirs = {"local": get_test_config_dir("media")}
     hass.config.skip_pip = True
     hass.config.skip_pip_packages = []
 
@@ -332,28 +336,60 @@ async def async_test_home_assistant_min_version(
         hass,
         {"_": ("Not empty or else some bad checks for hass config in discovery.py" " breaks")},
     )
+    hass.bus.async_listen_once(
+        EVENT_HOMEASSISTANT_STOP,
+        hass.config_entries._async_shutdown,
+        run_immediately=True,
+    )
 
     # Load the registries
     entity.async_setup(hass)
+    loader.async_setup(hass)
+
+    # setup translation cache instead of calling translation.async_setup(hass)
+    hass.data[translation.TRANSLATION_FLATTEN_CACHE] = translation._TranslationCache(hass)
     if load_registries:
         with (
-            patch("homeassistant.helpers.storage.Store.async_load", return_value=None),
+            patch.object(StoreWithoutWriteLoad, "async_load", return_value=None),
+            patch(
+                "homeassistant.helpers.area_registry.AreaRegistryStore",
+                StoreWithoutWriteLoad,
+            ),
+            patch(
+                "homeassistant.helpers.device_registry.DeviceRegistryStore",
+                StoreWithoutWriteLoad,
+            ),
+            patch(
+                "homeassistant.helpers.entity_registry.EntityRegistryStore",
+                StoreWithoutWriteLoad,
+            ),
+            patch(
+                "homeassistant.helpers.storage.Store",  # Floor & label registry are different
+                StoreWithoutWriteLoad,
+            ),
+            patch(
+                "homeassistant.helpers.issue_registry.IssueRegistryStore",
+                StoreWithoutWriteLoad,
+            ),
             patch(
                 "homeassistant.helpers.restore_state.RestoreStateData.async_setup_dump",
                 return_value=None,
             ),
-            patch("homeassistant.helpers.restore_state.start.async_at_start"),
+            patch(
+                "homeassistant.helpers.restore_state.start.async_at_start",
+            ),
         ):
-            await asyncio.gather(
-                ar.async_load(hass),
-                dr.async_load(hass),
-                er.async_load(hass),
-                ir.async_load(hass),
-                rs.async_load(hass),
-            )
+            await ar.async_load(hass)
+            await cr.async_load(hass)
+            await dr.async_load(hass)
+            await er.async_load(hass)
+            await fr.async_load(hass)
+            await ir.async_load(hass)
+            await lr.async_load(hass)
+            await rs.async_load(hass)
         hass.data[bootstrap.DATA_REGISTRIES_LOADED] = None
 
-    hass.state = CoreState.running
+    hass.set_state(CoreState.running)
 
     @callback
     def clear_instance(event):
@@ -362,7 +398,10 @@ async def async_test_home_assistant_min_version(
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_CLOSE, clear_instance)
 
-    return hass
+    yield hass
+
+    # Restore timezone, it is set when creating the hass object
+    dt_util.DEFAULT_TIME_ZONE = orig_tz
 
 
 @asynccontextmanager
@@ -376,15 +415,6 @@ async def async_test_home_assistant_dev(
     This should be copied from latest Home Assistant version,
     currently Home Assistant Core 2024.5.0dev0 (2024-4-11).
     """
-
-    # Local imports of features not present in min version
-    from homeassistant.helpers import (
-        category_registry as cr,
-        floor_registry as fr,
-        label_registry as lr,
-        translation,
-    )
-
     hass = HomeAssistant(config_dir or get_test_config_dir())
     store = auth_store.AuthStore(hass)
     hass.auth = auth.AuthManager(hass, store, {}, {})

70aa89c7a452cb16d5f1e23134c40b1b61988da4
Joakim Sørensen <ludeeus@ludeeus.dev>
2024-05-07 14:52:47 +0200

    Replace deprecated hass.components usage (#3528)
    * Replace deprecated hass.components usage
    
    * lint
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * Update custom_components/hacs/base.py
    
    * Restrict to 0 reported issues
    
    * Revert "Restrict to 0 reported issues"
    
    This reverts commit 19e3f1ac4507855effa14c28247123651e8647aa.
    
    ---------
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- custom_components/hacs/base.py
+++ custom_components/hacs/base.py
@@ -24,6 +24,9 @@ from aiogithubapi import (
 from aiogithubapi.objects.repository import AIOGitHubAPIRepository
 from aiohttp.client import ClientSession, ClientTimeout
 from awesomeversion import AwesomeVersion
+from homeassistant.components.persistent_notification import (
+    async_create as async_create_persistent_notification,
+)
 from homeassistant.config_entries import ConfigEntry, ConfigEntryState
 from homeassistant.const import EVENT_HOMEASSISTANT_FINAL_WRITE, Platform
 from homeassistant.core import HomeAssistant, callback
@@ -628,8 +631,8 @@ class HacsBase:
             for repo in critical:
                 if not repo["acknowledged"]:
                     self.log.critical("URGENT!: Check the HACS panel!")
-                    self.hass.components.persistent_notification.create(
-                        title="URGENT!", message="**Check the HACS panel!**"
+                    async_create_persistent_notification(
+                        self.hass, title="URGENT!", message="**Check the HACS panel!**"
                     )
                     break
 

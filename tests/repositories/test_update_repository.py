5603b66a7e5155351675eb18d39aafa8ef816e0c
ludeeus <joasoe@gmail.com>
2024-08-19 11:14:14 +0000

    Raise an error when update service target the downloaded version

--- tests/repositories/test_update_repository.py
+++ tests/repositories/test_update_repository.py
@@ -57,7 +57,7 @@ async def test_update_repository_entity(
     await snapshots.assert_hacs_data(
         hacs,
         f"{category_test_data['repository']
-                 }/test_update_repository_entity.json",
+           }/test_update_repository_entity.json",
     )
 
 
@@ -87,7 +87,7 @@ async def test_update_repository_websocket(
     await snapshots.assert_hacs_data(
         hacs,
         f"{category_test_data['repository']
-                 }/test_update_repository_websocket.json",
+           }/test_update_repository_websocket.json",
     )
 
 
@@ -254,5 +254,76 @@ async def test_update_repository_entity_download_failure(
             "update",
             "install",
             service_data={"entity_id": entity_id, "version": "2.0.0"},
+            blocking=True,
+        )
+
+
+async def test_update_repository_entity_same_provided_version(
+    hass: HomeAssistant, setup_integration: Generator
+):
+    hacs = get_hacs(hass)
+    repo = hacs.repositories.get_by_full_name("hacs-test-org/integration-basic")
+
+    assert repo is not None
+
+    repo.data.installed = True
+    repo.data.installed_version = "2.0.0"
+
+    await hass.config_entries.async_reload(hacs.configuration.config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    # Get a new HACS instance after reload
+    hacs = get_hacs(hass)
+
+    er = async_get_entity_registry(hacs.hass)
+
+    entity_id = er.async_get_entity_id("update", DOMAIN, repo.data.id)
+
+    with pytest.raises(
+        HomeAssistantError,
+        match=re.escape(
+            "Version 2.0.0 of hacs-test-org/integration-basic is already downloaded",
+        ),
+    ):
+        await hass.services.async_call(
+            "update",
+            "install",
+            service_data={"entity_id": entity_id, "version": "2.0.0"},
+            blocking=True,
+        )
+
+
+async def test_update_repository_entity_no_update(
+    hass: HomeAssistant,
+    setup_integration: Generator,
+):
+    hacs = get_hacs(hass)
+    repo = hacs.repositories.get_by_full_name("hacs-test-org/integration-basic")
+
+    assert repo is not None
+
+    repo.data.installed = True
+    repo.data.installed_version = "1.0.0"
+
+    await hass.config_entries.async_reload(hacs.configuration.config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    # Get a new HACS instance after reload
+    hacs = get_hacs(hass)
+
+    er = async_get_entity_registry(hacs.hass)
+
+    entity_id = er.async_get_entity_id("update", DOMAIN, repo.data.id)
+
+    with pytest.raises(
+        HomeAssistantError,
+        match=re.escape(
+            "No update available for update.basic_integration_update",
+        ),
+    ):
+        await hass.services.async_call(
+            "update",
+            "install",
+            service_data={"entity_id": entity_id},
             blocking=True,
         )

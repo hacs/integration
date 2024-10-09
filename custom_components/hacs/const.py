aae467b08a13004d0fba7d704ec930e82450aad7
KOMAD-bit <katharina.boehm.klamt@gmail.com>
2024-10-09 13:34:57 +0200

    test if soon everything works

--- custom_components/hacs/const.py
+++ custom_components/hacs/const.py
@@ -7,7 +7,7 @@ from aiogithubapi.common.const import ACCEPT_HEADERS
 NAME_SHORT = "HACS"
 DOMAIN = "hacs"
 CLIENT_ID = "395a8e669c5de9f7c6e8"
-MINIMUM_HA_VERSION = "0.0.0"
+MINIMUM_HA_VERSION = "2024.4.1"
 
 URL_BASE = "/hacsfiles"
 

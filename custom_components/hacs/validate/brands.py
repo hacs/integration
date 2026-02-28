ba1100f553e84d76c759a03b34b76ba2f4143812
Joakim Sørensen <joasoe@proton.me>
2026-02-28 08:54:44 +0100

    Allow local brands assets in the brands validator (#5128)
    * Allow local brands assets in the brands validator
    
    * Add warning
    
    * brand not brands
    
    * Refine logging messages for brand asset validation

--- custom_components/hacs/validate/brands.py
+++ custom_components/hacs/validate/brands.py
@@ -10,6 +10,7 @@ if TYPE_CHECKING:
     from ..repositories.base import HacsRepository
 
 URL = "https://brands.home-assistant.io/domains.json"
+ASSET_FILENAME = "icon.png"
 
 
 async def async_setup_validator(repository: HacsRepository) -> Validator:
@@ -26,10 +27,30 @@ class Validator(ActionValidationBase):
     async def async_validate(self) -> None:
         """Validate the repository."""
 
+        treefiles = self.repository.treefiles
+
+        if self.repository.repository_manifest.content_in_root:
+            asset_path = f"brand/{ASSET_FILENAME}"
+        else:
+            asset_path = f"{self.repository.content.path.remote}/brand/{ASSET_FILENAME}"
+
+        # Check if the integraiton provides local brand assets
+        if asset_path in treefiles:
+            self.repository.logger.debug("The repository contains brands assets at %s", asset_path)
+            return
+
+        self.repository.logger.warning(
+            "The repository does not contain brands assets at %s. "
+            "Falling back to checking the brands repository.",
+            asset_path,
+        )
+
+        # Fallback the checking the Home Assistant brands repository for the domain
         response = await self.hacs.session.get(URL)
         content = await response.json()
 
         if self.repository.data.domain not in content["custom"]:
             raise ValidationException(
-                "The repository has not been added as a custom domain to the brands repo"
+                "The repository does not provide brand assets "
+                "and is not listed in the Home Assistant brands repository."
             )
